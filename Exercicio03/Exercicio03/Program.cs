6673d09f85ad99124c8883fc78cd9c45f73ab6f8
Diego Andrade <dcandrade19@outlook.com>
2018-05-18 15:49:48 -0300

    teste3

--- Exercicio03/Exercicio03/Program.cs
+++ Exercicio03/Exercicio03/Program.cs
@@ -10,7 +10,7 @@ namespace Exercicio03
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Teste");
+            Console.WriteLine("Teste2");
             Console.ReadKey();
         }
     }
