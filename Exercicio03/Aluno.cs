80e2a956c1031bd84f3f3adabab48b54247123de
Diego Andrade <dcandrade19@outlook.com>
2018-05-18 15:41:57 -0300

    Teste

---
+++ Exercicio03/Aluno.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio03
+{
+    class Aluno
+    {
+        private string nome;
+        private string cpf;
+        private DateTime nascimento;
+
+        public Aluno(string nome, string cpf, DateTime nascimento)
+        {
+            this.nome = nome;
+            this.cpf = cpf;
+            this.nascimento = nascimento;
+        }
+
+        public string Nome { get; set; }
+
+        public string Cpf { get; set; }
+
+        public DateTime Nascimento { get; set; }
+    }
+}
